1826e4d027d6586bf4710d2b621c62364db6d035
Minh Pham <minh@nimblehq.co>
2022-11-30 15:39:43 +0700

    [#18] Revert `swift/Appfile.swift`, remove redundant lint steps and update the Configurations folder position

--- fastlane/swift/Appfile.swift
+++ fastlane/swift/Appfile.swift
@@ -1,10 +1,10 @@
 // Appfile.swift
 // Copyright (c) 2022 FastlaneTools
 
-var appIdentifier: String { return "co.nimblehq.crypto-prices" } // The bundle identifier of your app
-var appleID: String { return "minh@nimblehq.co" } // Your Apple Developer Portal username
+var appIdentifier: String { return "" } // The bundle identifier of your app
+var appleID: String { return "" } // Your Apple Developer Portal username
 
-var teamID: String { return "4TWS7E2EPE" } // Developer Portal Team ID
+var teamID: String { return "" } // Developer Portal Team ID
 var itcTeam: String? { return nil } // App Store Connect Team ID (may be nil if no team)
 
 // you can even provide different app identifiers, Apple IDs and team names per lane:
