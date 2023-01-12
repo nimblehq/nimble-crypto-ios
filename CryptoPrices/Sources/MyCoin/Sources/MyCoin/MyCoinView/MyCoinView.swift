5ce55235c3b09c1587f4794b1d7c66269ec5dab2
nkhanh44 <nkhanh4496@gmail.com>
2023-01-12 15:06:34 +0700

    [#63] Integrate pull to refresh

--- CryptoPrices/Sources/MyCoin/Sources/MyCoin/MyCoinView/MyCoinView.swift
+++ CryptoPrices/Sources/MyCoin/Sources/MyCoin/MyCoinView/MyCoinView.swift
@@ -42,7 +42,7 @@ public struct MyCoinView: View {
             await viewModel.fetchData(id: myCoinState.id)
         }
         .refreshable {
-            await viewModel.fetchAllData(id: myCoinState.id)
+            await viewModel.fetchData(id: myCoinState.id)
         }
     }
 
